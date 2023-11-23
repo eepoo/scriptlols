6fc4e1798c7cc2dcdcaf10c92203c9f94dad3514
eepoo <eepoo@users.noreply.github.com>
2023-11-23 13:36:00 +0200

    Update myscriptlets.js

--- myscriptlets.js
+++ myscriptlets.js
@@ -1,14 +1,14 @@
 (() => {
-	'use strict';
-	const vanha = findElementById("sb_form_q");
-	let uus = createElement("textarea");
+    'use strict';
+    const vanha = document.getElementById("sb_form_q");
+    let uus = document.createElement("textarea");
 
-	Array.from(vanha.attributes).forEach(attribute => {
-    		uus.setAttribute(
-      		attribute.nodeName === 'id' ? 'data-id' : attribute.nodeName,
-      		attribute.nodeValue,
-    		);
-  	});
+    Array.from(vanha.attributes).forEach(attribute => {
+            uus.setAttribute(
+              attribute.nodeName === 'id' ? 'data-id' : attribute.nodeName,
+              attribute.nodeValue,
+            );
+      });
 
-	vanha.replaceWith(uus);
+    vanha.replaceWith(uus);
 })();
